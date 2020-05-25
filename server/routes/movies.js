0bc466aacd922836560afafdddcf0229651f2d80
Mathias Bast <mathiasbast123@gmail.com>
2020-05-25 12:33:57 +1200

    adds stuff

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const request = require('superagent')
 
-router.get('/:title')
+router.get('/:title', (req,res) => {
+  const title = req.params.title
+  return request.get(`http://omdbapi.com/?apikey=69495595&s=${title}`)
+    .then(apiRes => {
+      res.send(JSON.parse(apiRes.text))
+      
+    })
+})
 
 module.exports = router
