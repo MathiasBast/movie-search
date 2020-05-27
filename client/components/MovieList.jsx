f35865b98019469988c983be0e0c04e50272fd60
Mathias Bast <mathiasbast123@gmail.com>
2020-05-27 12:34:15 +1200

    adds movie list thang

---
+++ client/components/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MovieList extends React.Component {
+  render () {
+    return (
+    <>
+    <h1>Hithere</h1>
+    </>
+    )
+  }
+}
+
+export default MovieList
