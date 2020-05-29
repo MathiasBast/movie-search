b4a5695cbe324a8856111ed4b7f0eff255365aaa
Mathias Bast <mathiasbast123@gmail.com>
2020-05-29 16:46:17 +1200

    adds poster, title and year

--- client/components/MovieList.jsx
+++ client/components/MovieList.jsx
@@ -5,8 +5,19 @@ class MovieList extends React.Component {
   render () {
     return (
     <>
-    {console.log(this.props)}
-    <h1>Hithere</h1>
+    {console.log(this.props.movies)}
+      {this.props.movies.length !== 0
+        ? <>
+        {this.props.movies.Search.map(movie => {
+          return <div key={movie.imbdId}>
+            <h2>{movie.Title}</h2>
+            <p>{movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title} />
+          </div>
+        })}
+        </>
+
+        : <h2>Search A Movie</h2>}
     </>
     )
   }
