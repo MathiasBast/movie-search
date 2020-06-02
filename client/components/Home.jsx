3339be8d5867cd043d680dfaae2824f05787904e
Mathias Bast <mathiasbast123@gmail.com>
2020-06-02 15:01:10 +1200

    it on enter

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { getMovies } from '../actions/movies'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 class Home extends React.Component {
   state={
-    title: ''
+    title: '',
+    search: false
   }
   titleHandler = event => {
     this.setState({
@@ -22,7 +23,8 @@ class Home extends React.Component {
   keyHandler = event => {
     event.preventDefault()
     if (event.keyCode === 13) {
-      this.props.dispatch(getMovies(this.state.title))
+      this.props.dispatch(getMovies(this.state.title));
+      this.setState({ search: true })
     }
   }
   render () {
@@ -32,11 +34,12 @@ class Home extends React.Component {
           <form>
             <input onKeyUp={this.keyHandler} placeholder='movie title' value={this.state.title} onChange={this.titleHandler} />
             <Link to='/search'>
-              <button type='button' onClick={this.submitHandler} >Submit</button>
+              <button type='submit' onClick={this.submitHandler} >Submit</button>
             </Link>
           </form>
           <h2>Search A Movie</h2>
         </div>
+        {this.state.search && <Redirect to='/search' />}
       </>
     )
   }
