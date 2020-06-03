ad63deebb1a00b74fee5f5cffa4607306a22e16a
Mathias Bast <mathiasbast123@gmail.com>
2020-06-03 13:40:11 +1200

    it workssss!!!!

--- client/components/Pages.jsx
+++ client/components/Pages.jsx
@@ -3,12 +3,21 @@ import { connect } from 'react-redux'
 
 class Pages extends React.Component {
   pages (number) {
+    number = Number(number)
     if (number <= 10) {
+      // eslint-disable-next-line no-useless-return
       return
     } else {
-      return <h1>Hiiii</h1>
+      var final = []
+      const pages = Math.ceil(number / 10)
+      for (let i = 0; i < pages; i++) {
+        final.push(<button>{i + 1}</button>)
+      }
+      console.log(final)
+      return final
     }
   }
+
   render () {
     return (
       <>
