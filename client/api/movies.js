02bd0c8cd09c118e1ea8283ddfacfce09c642916
Mathias Bast <mathiasbast123@gmail.com>
2020-06-03 13:51:04 +1200

    set up front end api

--- client/api/movies.js
+++ client/api/movies.js
@@ -1,7 +1,14 @@
 import request from 'superagent'
 
 export function getMovies (title) {
-  return request.get(`/api/v1/${title}`)
+  return request.get(`/api/v1/movie/${title}`)
+    .then(res => {
+      return res
+    })
+}
+
+export function getMoviePage (page, movie) {
+  return request.get(`/api/v1/page/${page}/${movie}`)
     .then(res => {
       return res
     })
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const request = require('superagent')
 
-router.get('/:title', (req, res) => {
+router.get('/movie/:title', (req, res) => {
   const title = req.params.title
   return request.get(`http://omdbapi.com/?apikey=69495595&s=${title}`)
     .then(apiRes => {
